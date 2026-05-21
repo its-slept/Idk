a0819d79ec3407480a06ce631bf33dc6ba368d96
its-slept <noobyt143op@gmail.com>
2026-05-21 17:14:06 -0500

    add card 1256: Cyn #2

--- data/cards.js
+++ data/cards.js
@@ -10070,6 +10070,14 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-9/Cyn-1.webp"
+},{
+  "id": "1256",
+  "name": "Cyn #2",
+  "series": "Murder Drones",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-9/Cyn-2.webp"
 }
 ];
 
