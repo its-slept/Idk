8f70c0c3104de331f6f3997f365ee64b51079ae7
its-slept <noobyt143op@gmail.com>
2026-05-22 12:00:30 -0500

    edit card 176: imageUrl

--- data/cards.js
+++ data/cards.js
@@ -1453,7 +1453,7 @@ export const CARDS = [
   "rarity": "rare,epic,legendary",
   "type": "Permanent",
   "source": "Official Art",
-  "imageUrl": "https://raw.githubusercontent.com/its-slept/Idk/main/Base%20-%202/Picsart_26-01-31_04-56-44-408_1.png"
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-10/Mornye-1.webp"
 },{
   "id": "177",
   "name": "Mornye #2",
