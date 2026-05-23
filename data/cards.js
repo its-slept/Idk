88c90159bfe8faa452cc8c4bf34ec4c5b401863c
its-slept <noobyt143op@gmail.com>
2026-05-23 09:19:04 -0500

    edit card 60: imageUrl

--- data/cards.js
+++ data/cards.js
@@ -525,7 +525,7 @@ export const CARDS = [
   "rarity": "rare,epic,legendary",
   "type": "Permanent",
   "source": "Official Art",
-  "imageUrl": "https://raw.githubusercontent.com/its-slept/Idk/main/Base/Picsart_26-01-27_06-44-55-916.png"
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-11/Phainon-3.webp"
 },{
   "id": "61",
   "name": "Phainon #4",
