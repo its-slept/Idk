06cc08b169be915f16a64cb6eab74c80953e4597
its-slept <noobyt143op@gmail.com>
2026-05-23 12:53:07 -0500

    edit card 146: imageUrl

--- data/cards.js
+++ data/cards.js
@@ -1213,7 +1213,7 @@ export const CARDS = [
   "rarity": "rare,epic,legendary",
   "type": "Permanent",
   "source": "Official Art",
-  "imageUrl": "https://raw.githubusercontent.com/its-slept/Idk/main/Base%20-%202/Picsart_26-01-31_03-51-05-429_1.png"
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-11/Marin-Kitagawa-1.webp"
 },{
   "id": "147",
   "name": "Marin Kitagawa #2",
