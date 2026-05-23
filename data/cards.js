be575fbd8e568edf1ab0dddff0caf4aac540919c
its-slept <noobyt143op@gmail.com>
2026-05-23 08:57:47 -0500

    edit card 56: imageUrl

--- data/cards.js
+++ data/cards.js
@@ -493,7 +493,7 @@ export const CARDS = [
   "rarity": "rare,epic,legendary",
   "type": "Permanent",
   "source": "Official Art",
-  "imageUrl": "https://raw.githubusercontent.com/its-slept/Idk/main/Base/Picsart_26-01-27_06-33-59-860.png"
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-11/Kafka-5.webp"
 },{
   "id": "57",
   "name": "Kafka #6",
