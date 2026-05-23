6281d5ffd19a54e5b93cefb6aa556ff6933aff6a
its-slept <noobyt143op@gmail.com>
2026-05-23 05:27:02 -0500

    add card 1283: Krillin #1

--- data/cards.js
+++ data/cards.js
@@ -10286,6 +10286,14 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-10/Rachie-2.webp"
+},{
+  "id": "1283",
+  "name": "Krillin #1",
+  "series": "Dragon Ball Verse",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-10/Krillin-1.webp"
 }
 ];
 
