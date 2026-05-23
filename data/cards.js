040bf88add5dae949babdeec3925817fc8ec4792
its-slept <noobyt143op@gmail.com>
2026-05-23 06:26:22 -0500

    edit card 145: imageUrl

--- data/cards.js
+++ data/cards.js
@@ -1205,7 +1205,7 @@ export const CARDS = [
   "rarity": "rare,epic,legendary",
   "type": "Permanent",
   "source": "Official Art",
-  "imageUrl": "https://raw.githubusercontent.com/its-slept/Idk/main/Base%20-%202/Picsart_26-01-31_03-43-50-378_1.png"
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-11/Aquamarine-Hoshino-6.webp"
 },{
   "id": "146",
   "name": "Marin Kitagawa #1",
