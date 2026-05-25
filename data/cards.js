6801001700edd4b6e7738d3c07e5262f54bdaebc
its-slept <noobyt143op@gmail.com>
2026-05-25 16:11:27 -0500

    edit card 240: imageUrl

--- data/cards.js
+++ data/cards.js
@@ -1965,7 +1965,7 @@ export const CARDS = [
   "rarity": "rare,epic,legendary",
   "type": "Permanent",
   "source": "Official Art",
-  "imageUrl": "https://raw.githubusercontent.com/its-slept/Idk/main/Base%20-%202/Picsart_26-02-11_15-33-15-754.webp"
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-12/Kurumi-Tokisaki-3.webp"
 },{
   "id": "241",
   "name": "Kurumi Tokisaki #4",
