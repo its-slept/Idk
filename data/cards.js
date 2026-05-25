7e957727c62c458bb2d0baadc9e20af52943d4cc
its-slept <noobyt143op@gmail.com>
2026-05-25 06:37:29 -0500

    edit card 117: imageUrl

--- data/cards.js
+++ data/cards.js
@@ -981,7 +981,7 @@ export const CARDS = [
   "rarity": "rare,epic,legendary",
   "type": "Permanent",
   "source": "Official Art",
-  "imageUrl": "https://raw.githubusercontent.com/its-slept/Idk/main/Base%20-%202/Picsart_26-01-31_03-19-21-071_1.png"
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-12/Alisa-Mikhailovna-Kujou-2.webp"
 },{
   "id": "118",
   "name": "Alisa Mikhailovna Kujou #3",
