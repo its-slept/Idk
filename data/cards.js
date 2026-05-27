83077875f916a0d0b87cd4b56ad24b08542340ac
its-slept <noobyt143op@gmail.com>
2026-05-27 16:26:23 -0500

    add card 1343: Koseki Bijou #1

--- data/cards.js
+++ data/cards.js
@@ -10766,6 +10766,14 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-13/Mita-3.webp"
+},{
+  "id": "1343",
+  "name": "Koseki Bijou #1",
+  "series": "Hololive",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-13/Koseki-Bijou-1.webp"
 }
 ];
 
