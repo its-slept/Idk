1b64e0ceef311cb9768a744705698c1ec7f7f723
its-slept <noobyt143op@gmail.com>
2026-05-28 11:11:54 -0500

    add card 1367: Broly #2

--- data/cards.js
+++ data/cards.js
@@ -10958,6 +10958,14 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-13/Broly-1.webp"
+},{
+  "id": "1367",
+  "name": "Broly #2",
+  "series": "Dragon Ball Verse",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-13/Broly-2.webp"
 }
 ];
 
