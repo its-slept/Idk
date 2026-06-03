6a30f508c20f5ec6e537f4f55ea3b8b807ed0640
its-slept <noobyt143op@gmail.com>
2026-06-03 15:10:26 -0500

    add card 1405: Shion #1

--- data/cards.js
+++ data/cards.js
@@ -11262,6 +11262,14 @@ export const CARDS = [
   "type": "Permanent",
   "source": "Official Art",
   "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-14/Nezumi-1.webp"
+},{
+  "id": "1405",
+  "name": "Shion #1",
+  "series": "No. 6",
+  "rarity": "rare,epic,legendary",
+  "type": "Permanent",
+  "source": "Official Art",
+  "imageUrl": "https://raw.githubusercontent.com/its-slept/Card-bot/main/Permanent-14/Shion-1.webp"
 }
 ];
 
